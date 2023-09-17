c1c0333758cce9ae582386ed063e1bd133fd6e88
2yeram <ramramm_@naver.com>
2023-09-17 17:44:22 +0900

    [ring] 1 받으면 사진 뜨게끔

--- app/src/main/java/com/siyeon/haniumproject/notificationActivity.kt
+++ app/src/main/java/com/siyeon/haniumproject/notificationActivity.kt
@@ -1,26 +1,24 @@
 package com.siyeon.haniumproject
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
 
-class NotificationActivity : AppCompatActivity() {
+class notificationActivity : AppCompatActivity() {
 
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+        setContentView(R.layout.activity_main)
 
         FirebaseApp.initializeApp(this)
+
+        imageView = findViewById(R.id.test) // 이미지뷰
 
         // Firebase Realtime Database의 데이터를 가져올 레퍼런스 설정
         databaseReference = FirebaseDatabase.getInstance().getReference("test/push")
@@ -28,43 +26,21 @@ class NotificationActivity : AppCompatActivity() {
         // ValueEventListener를 사용하여 데이터 가져오기
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val pushValue = dataSnapshot.getValue(Long::class.java)?.toString()
+                val pushValue = dataSnapshot.getValue(Long::class.java)
 
-                if (pushValue == "1") {
-                    // 알림을 표시하는 코드 호출
-                    showNotification(applicationContext)
+                if (pushValue == 1L) {
+                    // pushValue가 1로 변경되었을 때 이미지를 표시
+                    imageView.setImageResource(R.drawable.ring_resize)
+                } else {
+                    // pushValue가 1이 아닌 경우 이미지를 숨김
+                    imageView.setImageDrawable(null)
                 }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
                 val errorMessage = databaseError.message
                 Log.e("FirebaseDatabase", "Firebase Database 오류: $errorMessage")
-                Toast.makeText(applicationContext, "Firebase Database 오류: $errorMessage", Toast.LENGTH_SHORT).show()
             }
         })
-    }
-
-    // 알림 표시 함수
-    private fun showNotification(context: Context) {
-        val builder = NotificationCompat.Builder(context, "MY_channel")
-            .setSmallIcon(R.drawable.cctv_resize) // 알림 아이콘 설정
-            .setContentTitle("알림 제목")
-            .setContentText("알림 내용")
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel_id = "MY_channel"
-            val channel_name = "DING DONG DANG"
-            val descriptionText = "키즈카페에서 위급 상황이 발생했습니다."
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(channel_id, channel_name, importance).apply {
-                description = descriptionText
-            }
-
-            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-
-            val notification = builder.build()
-            notificationManager.notify(1002, notification)
-        }
     }
 }
