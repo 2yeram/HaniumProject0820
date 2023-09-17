e5d15bbc4252286ad2799cd2cd8a1ebc2fa06c56
2yeram <ramramm_@naver.com>
2023-09-17 14:44:53 +0900

    [ring] 갤러리 버튼 누르면 알림 옴

--- app/src/main/java/com/siyeon/haniumproject/MainActivity.kt
+++ app/src/main/java/com/siyeon/haniumproject/MainActivity.kt
@@ -8,6 +8,12 @@ import android.widget.Button
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
+import android.os.Build
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.content.Context
+import androidx.core.app.NotificationCompat
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,8 +40,25 @@ class MainActivity : AppCompatActivity() {
 
         var buttonGalleryActivity = findViewById<Button>(R.id.bt_home_gallery)
         buttonGalleryActivity.setOnClickListener {
-            var intent = Intent(applicationContext, GalleryActivity::class.java)
-            startActivity(intent)
+            var builder = NotificationCompat.Builder(this, "MY_channel")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("알림 제목")
+                .setContentText("알림 내용")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel_id = "MY_channel" // 알림을 받을 채널 id 설정
+                val channel_name = "DING DONG DANG" // 채널 이름 설정
+                val descriptionText = "키즈카페에서 위험 상황이 발생했습니다." // 채널 설명글 설정
+                val importance = NotificationManager.IMPORTANCE_DEFAULT // 알림 우선순위 설정
+                val channel = NotificationChannel(channel_id, channel_name, importance).apply {
+                    description = descriptionText
+                }
+
+                // 알림 표시
+                val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+                notificationManager.notify(1002, builder.build())
+            }
         }
 
         var buttonFacilityActivity = findViewById<Button>(R.id.bt_home_facility)
